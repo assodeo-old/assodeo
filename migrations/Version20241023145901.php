c938aa5fbe044dc0623cca7787a638578dcea453
Juan Valero <159065266+juanvlro@users.noreply.github.com>
2024-10-23 21:34:49 +0200

    Fix review

--- migrations/Version20241023145901.php
+++ migrations/Version20241023145901.php
@@ -14,7 +14,7 @@ final class Version20241018092133 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add user table';
     }
 
     public function up(Schema $schema): void
